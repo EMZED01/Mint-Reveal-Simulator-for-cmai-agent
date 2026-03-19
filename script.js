12eef430343b9867869d14d2ed88dc334252986b
EMZED <130805296+EMZED01@users.noreply.github.com>
2026-03-19 06:33:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function mintNFT() {
     setTimeout(() => {
       const randomId = Math.floor(Math.random() * totalNFTs) + 1;
 
-      img.src = "nfts/" + randomId + ".png";
+      img.src = "nfts/" + randomId + ".jpeg";
 
       const rarityRoll = Math.random();
 
